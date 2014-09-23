6d406e9ec8f7b12e2fdc1900decba6e9e80d640a
DemaudeSven <sven.demaude@etud.infographie-sup.eu>
2014-09-23 14:11:45 +0200

    old woloo

---
+++ woloo/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+	int i = 0;
+
+	while (i != 100000000)
+	{
+
+		printf(" I luv ya Ambroise Gram %d Times \n", i );
+
+		i++;
+
+	}
+	// je commente :p
+
+
+}
